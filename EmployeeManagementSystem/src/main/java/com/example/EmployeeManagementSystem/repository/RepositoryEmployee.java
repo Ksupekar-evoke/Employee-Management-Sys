af309a022d2037ddb1ab255a9f355d1b08217896
Kunal Supekar <ksupekar@evoketechnologies.com>
2026-06-03 17:16:19 +0530

    Update RepositoryEmployee.java

--- EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/RepositoryEmployee.java
+++ EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/RepositoryEmployee.java
@@ -4,7 +4,7 @@ import com.example.EmployeeManagementSystem.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Reposi
 public interface RepositoryEmployee extends JpaRepository<Employee,Integer> {
 
 }
